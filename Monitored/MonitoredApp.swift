ff81f342789b56b0e87881725d163d4eaee678be
Pepperomai <akuhtarova@gmail.com>
2026-03-20 11:48:01 +0300

    Initial Commit

---
+++ Monitored/MonitoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonitoredApp.swift
+//  Monitored
+//
+//  Created by Анна Кухтарова on 20.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct MonitoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MonitoredTests/MonitoredTests.swift
@@ -0,0 +1,17 @@
+//
+//  MonitoredTests.swift
+//  MonitoredTests
+//
+//  Created by Анна Кухтарова on 20.03.2026.
+//
+
+import Testing
+@testable import Monitored
+
+struct MonitoredTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
